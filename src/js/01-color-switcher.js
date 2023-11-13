1f57c3c8839901ff0590a800a66c1c8d136139fc
Podmaskov <podmaskov@gmail.com>
2023-11-13 12:59:48 +0100

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const CHANGE_COLOR_DURATION = 1000;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+const startHandler = () => {
+  startBtn.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_DURATION);
+};
+
+const stopHandler = () => {
+  clearInterval(intervalId);
+  startBtn.removeAttribute('disabled', false);
+};
+
+startBtn.addEventListener('click', startHandler);
+
+stopBtn.addEventListener('click', stopHandler);
